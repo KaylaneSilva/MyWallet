19a4afbb0c66c06e7a3358208e58c7a2ca7ba54f
kaylanerss <kaylane25@hotmail.com>
2022-02-13 22:59:38 -0300

    inciando build da aplicacao

---
+++ build/precache-manifest.b1e523804e5023900c24fc5151250070.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c12f012ad90f3a6b599404c1b0ebd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "088e5912741e9f1b6773",
+    "url": "/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "8b53667321903efb871c",
+    "url": "/static/css/main.eee5b12a.chunk.css"
+  },
+  {
+    "revision": "088e5912741e9f1b6773",
+    "url": "/static/js/2.c547303b.chunk.js"
+  },
+  {
+    "revision": "313dcef98ad920c9c4fa701c6c12e372",
+    "url": "/static/js/2.c547303b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b53667321903efb871c",
+    "url": "/static/js/main.8c101ec6.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  },
+  {
+    "revision": "ffe434a90a61ff1b174a5a1449564dba",
+    "url": "/static/media/pexels-john-guccione-wwwadvergroupcom-3531895.ffe434a9.jpg"
+  },
+  {
+    "revision": "e1f33f3aa8fb6e3b8607c8f35633c0d8",
+    "url": "/static/media/pexels-pixabay-259209.e1f33f3a.jpg"
+  },
+  {
+    "revision": "fea8b2befb2ecd2461b6a576bbffa9af",
+    "url": "/static/media/wallet.fea8b2be.png"
+  },
+  {
+    "revision": "4d2397ea9b709eb2fdf4b3986518da8d",
+    "url": "/static/media/walletWhite.4d2397ea.svg"
+  }
+]);
