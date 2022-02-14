36cc4332d4370c3f5063726e11df6fbd8b4b6c79
kaylanerss <kaylane25@hotmail.com>
2022-02-14 12:44:37 -0300

    arrumando responsividade do botao

--- src/components/FormEdit.js
+++ src/components/FormEdit.js
@@ -73,100 +73,102 @@ class Forms extends React.Component {
       moedas,
     } = this.state;
     return (
-      <div className="div-Info">
-        <div className="input-group mb-3">
-          <span html="description" className="input-group-text">
-            Descrição
-          </span>
-          <input
-            name="description"
-            id="description"
-            type="text"
-            value={ description }
-            onChange={ this.handleChange }
-            data-testid="description-input"
-          />
-        </div>
-        <div className="input-group input-group-sm mb-3">
-          <span html="valor" className="input-group-text">
-            Valor
-          </span>
-          <input
-            name="value"
-            id="valor"
-            type="number"
-            data-testid="value-input"
-            value={ value }
-            onChange={ this.handleChange }
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span htmlFor="method" className="input-group-text">
-            Pagamento
-          </span>
-          <select
-            className="form-select"
-            name="method"
-            value={ method }
-            id="method"
-            onChange={ this.handleChange }
-            data-testid="method-input"
-          >
-            <option> </option>
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
-        </div>
-        <div className="input-group mb-3">
-          <span htmlFor="tag" className="input-group-text">
-            Tag
-          </span>
-          <select
-            className="form-select"
-            name="tag"
-            value={ tag }
-            id="tag"
-            onChange={ this.handleChange }
-            data-testid="tag-input"
-          >
-            <option> </option>
-            <option value="Alimentção">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
-        </div>
-        <div className="input-group mb-3">
-          <label htmlFor="currency" className="coin">
-            <span className="input-group-text">
-              Moeda
+      <div className='container-info'>
+        <section className="div-Info">
+          <div className="input-group mb-3">
+            <span html="description" className="input-group-text">
+              Descrição
+            </span>
+            <input
+              name="description"
+              id="description"
+              type="text"
+              value={ description }
+              onChange={ this.handleChange }
+              data-testid="description-input"
+            />
+          </div>
+          <div className="input-group input-group-sm mb-3">
+            <span html="valor" className="input-group-text">
+              Valor
+            </span>
+            <input
+              name="value"
+              id="valor"
+              type="number"
+              data-testid="value-input"
+              value={ value }
+              onChange={ this.handleChange }
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span htmlFor="method" className="input-group-text">
+              Pagamento
             </span>
             <select
               className="form-select"
-              name="currency"
-              id="currency"
-              value={ currency }
+              name="method"
+              value={ method }
+              id="method"
               onChange={ this.handleChange }
-              data-testid="currency-input"
+              data-testid="method-input"
             >
-              { moedas ? moedas.map((coin, index) => {
-                if (coin.code === 'USDT' || coin.codein === 'BRLT') {
-                  return '';
-                }
-                return (
-                  <option
-                    key={ index }
-                    value={ coin.code }
-                    data-testid={ coin.code }
-                  >
-                    { coin.code }
-                  </option>);
-              }) : ''}
+              <option> </option>
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
             </select>
-          </label>
-        </div>
+          </div>
+          <div className="input-group mb-3">
+            <span htmlFor="tag" className="input-group-text">
+              Tag
+            </span>
+            <select
+              className="form-select"
+              name="tag"
+              value={ tag }
+              id="tag"
+              onChange={ this.handleChange }
+              data-testid="tag-input"
+            >
+              <option> </option>
+              <option value="Alimentção">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </div>
+          <div className="input-group mb-3">
+            <label htmlFor="currency" className="coin">
+              <span className="input-group-text">
+                Moeda
+              </span>
+              <select
+                className="form-select"
+                name="currency"
+                id="currency"
+                value={ currency }
+                onChange={ this.handleChange }
+                data-testid="currency-input"
+              >
+                { moedas ? moedas.map((coin, index) => {
+                  if (coin.code === 'USDT' || coin.codein === 'BRLT') {
+                    return '';
+                  }
+                  return (
+                    <option
+                      key={ index }
+                      value={ coin.code }
+                      data-testid={ coin.code }
+                    >
+                      { coin.code }
+                    </option>);
+                }) : ''}
+              </select>
+            </label>
+          </div>
+        </section>
         <button
           className="btn btn-primary btn-sm"
           type="button"
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -53,100 +53,102 @@ class Forms extends React.Component {
       method,
     } = this.state;
     return (
-      <div className="div-putInfo">
-        <div className="input-group mb-3">
-          <span html="description" className="input-group-text">
-            Descrição
-          </span>
-          <input
-            name="description"
-            id="description"
-            type="text"
-            value={description}
-            onChange={this.handleChange}
-            data-testid="description-input"
-          />
-        </div>
-        <div className="input-group input-group-sm mb-3">
-          <span html="valor" className="input-group-text">
-            Valor
-          </span>
-          <input
-            name="value"
-            id="valor"
-            type="number"
-            data-testid="value-input"
-            value={value}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span htmlFor="method" className="input-group-text">
-            Pagamento
-          </span>
-          <select
-            className="form-select"
-            name="method"
-            value={method}
-            id="method"
-            onChange={this.handleChange}
-            data-testid="method-input"
-          >
-            <option> </option>
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
-        </div>
-        <div className="input-group mb-3">
-          <span htmlFor="tag" className="input-group-text">
-            Tag
-          </span>
-          <select
-            className="form-select"
-            name="tag"
-            value={tag}
-            id="tag"
-            onChange={this.handleChange}
-            data-testid="tag-input"
-          >
-            <option> </option>
-            <option value="Alimentção">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
-        </div>
-        <div className="input-group mb-3">
-          <label htmlFor="currency" className="label-coin">
-            <span className="input-group-text">
-              Moeda
+      <div className='info-container'>
+        <section className="div-putInfo">
+          <div className="input-group mb-3">
+            <span html="description" className="input-group-text">
+              Descrição
+            </span>
+            <input
+              name="description"
+              id="description"
+              type="text"
+              value={description}
+              onChange={this.handleChange}
+              data-testid="description-input"
+            />
+          </div>
+          <div className="input-group input-group-sm mb-3">
+            <span html="valor" className="input-group-text">
+              Valor
+            </span>
+            <input
+              name="value"
+              id="valor"
+              type="number"
+              data-testid="value-input"
+              value={value}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span htmlFor="method" className="input-group-text">
+              Pagamento
             </span>
             <select
               className="form-select"
-              name="currency"
-              id="currency"
-              value={currency}
+              name="method"
+              value={method}
+              id="method"
               onChange={this.handleChange}
-              data-testid="currency-input"
+              data-testid="method-input"
             >
-              {currencies ? currencies.map((coin, index) => {
-                if (coin === 'USDT' || coin === 'BRLT') {
-                  return '';
-                }
-                return (
-                  <option
-                    key={index}
-                    value={coin}
-                    data-testid={coin}
-                  >
-                    {coin}
-                  </option>);
-              }) : ''}
+              <option> </option>
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
             </select>
-          </label>
-        </div>
+          </div>
+          <div className="input-group mb-3">
+            <span htmlFor="tag" className="input-group-text">
+              Tag
+            </span>
+            <select
+              className="form-select"
+              name="tag"
+              value={tag}
+              id="tag"
+              onChange={this.handleChange}
+              data-testid="tag-input"
+            >
+              <option> </option>
+              <option value="Alimentção">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </div>
+          <div className="input-group mb-3">
+            <label htmlFor="currency" className="label-coin">
+              <span className="input-group-text">
+                Moeda
+              </span>
+              <select
+                className="form-select"
+                name="currency"
+                id="currency"
+                value={currency}
+                onChange={this.handleChange}
+                data-testid="currency-input"
+              >
+                {currencies ? currencies.map((coin, index) => {
+                  if (coin === 'USDT' || coin === 'BRLT') {
+                    return '';
+                  }
+                  return (
+                    <option
+                      key={index}
+                      value={coin}
+                      data-testid={coin}
+                    >
+                      {coin}
+                    </option>);
+                }) : ''}
+              </select>
+            </label>
+          </div>
+        </section>
         <button
           className="btn btn-primary btn-sm"
           type="button"
